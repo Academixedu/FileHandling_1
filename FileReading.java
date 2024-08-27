0ef649e08a6ba28fa7236aeccddb2526e3833f37
ganeshteki <ganeshteki2001@gmail.com>
2024-08-27 18:54:05 +0530

    filehandling

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 public class FileReading{
 // Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+ public static void main(String[] args) throws IOException {
+    FileReader fr=new FileReader("C:\\Users\\ganes\\Desktop\\java basics\\FileHandling_1\\Files.txt");
+    BufferedReader br=new BufferedReader(fr);
+
+            String name="";
+            while((name=br.readLine())!=null){
+                System.out.println(name);
+
+            }
+
+
+
+
+br.close();
+ }
 }
+
--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,19 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+public static void main(String[] args) throws IOException {
+    
+
+FileWriter fw= new FileWriter("C:\\Users\\ganes\\Desktop\\java basics\\FileHandling_1\\Files.txt");
+ BufferedWriter bw=new BufferedWriter(fw);
+  bw.write("pak");
+  bw.newLine();
+  bw.write("gani");
+  bw.newLine();
+bw.close();
+}
 }
