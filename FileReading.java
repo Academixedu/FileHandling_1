624909f5e789657a334eabaf723dee4f3774a716
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-27 18:10:18 +0530

    Swarupa

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReading{
 // Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+public static void main(String[] args) throws IOException {
+    
+
+FileReader f1=new FileReader("C:\\Users\\Swarupa Jangam\\OneDrive\\Desktop\\Buffers\\FileHandling_1\\Files.txt");
+ BufferedReader b1=new BufferedReader(f1);
+ String data="";
+ while((data=b1.readLine())!=null){
+    System.out.println(data);
+
+ }
+b1.close();
+
+}
 }
--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+public static void main(String[] args) throws IOException{
+    FileWriter f=new FileWriter("C:\\Users\\Swarupa Jangam\\OneDrive\\Desktop\\Buffers\\FileHandling_1\\Files.txt");
+    BufferedWriter b=new BufferedWriter(f);
+    b.write("Swarupa\n");
+    b.write("Saarah\n");
+    b.write("Sanjana\n");
+    b.close();
+
+
+}
 }
