64978a496ac625f08bf17856eeeda166986fce10
Swapna-321 <gowdaperuswapna@gmail.com>
2024-08-27 11:03:27 -0400

    file

--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+public static void main(String args[]) throws IOException{
+    FileWriter fr = new FileWriter("E:\nayeem\Assignments\assignment 26\Files.txt");
+    BufferedWriter br = new BufferedWriter(fr);
+    br.write("Swapna");
+    br.newLine();
+    br.write("Swathi");
+    br.newLine();
+    br.write("Rocky");
+    br.close();
+}
 }
