cf2911e0155eb9b201af07d0485316dd0c3821b7
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-27 17:54:49 +0530

    kavitha

--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,20 @@
+
+import java.io.IOException;
+import java.io.BufferedWriter;
+import  java.io.FileWriter;;
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+public static void main(String[] args) throws IOException
+{
+    
+    FileWriter in = new FileWriter("C:\\Users\\gkavi\\OneDrive\\Desktop\\untroll\\FileHandling_1\\Files.txt");
+    BufferedWriter b= new BufferedWriter(in);
+    b.write("kavitha");
+    b.newLine();
+    b.write("sujatha");
+    b.newLine();
+    b.write("swarupa");
+    b.newLine();
+    b.close();
+}
 }
