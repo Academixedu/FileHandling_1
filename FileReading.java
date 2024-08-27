5d9f759913d853aaa137ac2eca9caec8ef6c246b
Shreyash000007 <shreyasharg@gmail.com>
2024-08-27 18:37:04 +0530

    Shreyash

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,18 @@
-public class FileReading{
-// Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReading {
+
+    public static void main(String[] args) throws IOException {      
+        FileReader fr = new FileReader("E:\\File Handling\\File Handling\\FileHandling_1\\Files.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        
+        br.close();
+    }
 }
