af910fac11ce4821855fec00230013f4b7dbd65c
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-08-27 18:32:43 +0530

    done

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class FileReading{
 // Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+    public static void main(String[] args) throws Exception {
+        FileReader fr=new FileReader("C:\\academix\\java basics\\FileHandling_1\\Files.txt");
+        BufferedReader br=new BufferedReader(fr);
+        String li="";
+        while((li=br.readLine())!=null){
+            System.err.println(li);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        FileWriter fw=new FileWriter("C:\\academix\\java basics\\FileHandling_1\\Files.txt");
+        BufferedWriter bw=new BufferedWriter(fw);
+        bw.write("import languages to learn");
+        bw.newLine();
+        for(int i=1;i<=5;i++){
+            System.out.println("Enter language " + i + ":");
+
+            String lang=sc.nextLine();
+            bw.write(lang);
+            bw.newLine();
+        }
+        bw.close();
+    }
 }
