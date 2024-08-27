8349eca2e90f0ca99e3604f1ec826f53f13ff686
nayeem6836 <maseednayeem@gmail.com>
2024-08-27 20:07:43 +0530

    Update FileWriting.java

--- FileWriting.java
+++ FileWriting.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
 public static void main(String args[]) throws IOException{
-    FileWriter fr = new FileWriter("C:\\Users\\Karthik\\Documents\\Assignments\\FileHandling_1\\Files.txt");
+    FileWriter fr = new FileWriter("E:\nayeem\Assignments\assignment 26\Files.txt");
     BufferedWriter br = new BufferedWriter(fr);
     br.write("Maseed");
     br.newLine();
