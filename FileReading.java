ea350a7f0972cf14571305860cc0f81598b98deb
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-27 18:03:55 +0530

    filehandling

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,27 @@
-public class FileReading{
-// Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+import java.io.*;
+
+public class FileReading {
+    public static void main(String[] args) {
+        try {
+            // FileReader to read the file
+            FileReader br = new FileReader("C:/Users/mekam/OneDrive/Desktop/Java/FileHandling_1/Files.txt");
+            BufferedReader bf = new BufferedReader(br);
+            
+            String str = bf.readLine();
+            while (str != null) {
+                System.out.println(str);
+                str = bf.readLine();  // Read next line
+            }
+            
+            bf.close();  // Close BufferedReader
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+           
+           
+        } 
+        
+    }
 }
--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,16 @@
-public class FileWriting{
-// Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+import java.io.*;
+
+public class FileWriting {
+    public static void main(String[] args) throws IOException {
+        FileWriter f = new FileWriter("C:/Users/mekam/OneDrive/Desktop/Java/FileHandling_1/Files.txt");
+        BufferedWriter b = new BufferedWriter(f);
+        
+        b.write("this is file writing");
+        b.newLine();
+        b.write("this is second line");
+        b.newLine();
+        
+        b.close();
+        System.out.println("File Written Successfully");
+    }
 }
