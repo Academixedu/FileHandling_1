fe1e7c64a77afe2572244fc1201937a7d592a1e1
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-27 18:02:30 +0530

    kavitha

--- FileWriting.java
+++ FileWriting.java
@@ -9,12 +9,9 @@ public static void main(String[] args) throws IOException
     
     FileWriter in = new FileWriter("C:\\Users\\gkavi\\OneDrive\\Desktop\\untroll\\FileHandling_1\\Files.txt");
     BufferedWriter b= new BufferedWriter(in);
-    b.write("kavitha");
-    b.newLine();
-    b.write("sujatha");
-    b.newLine();
-    b.write("swarupa");
-    b.newLine();
+    b.write("kavitha\n");
+    b.write("sujatha\n");
+    b.write("swarupa\n");
     b.close();
 }
 }
