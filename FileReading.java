fa658507ac98aff83667791df1e0a09bc8493bf4
sujatha-94 <sujathakarri701@gmail.com>
2024-08-27 18:08:12 +0530

    programs

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReading{
 // Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+public static void main(String[] args) throws IOException {
+    FileReader f = new FileReader("C:\\Users\\sujat\\OneDrive\\Desktop\\s\\FileHandling_1\\Files.txt");
+    BufferedReader b= new BufferedReader(f);
+    String line ="";
+    while ((line = b.readLine()) != null) {
+        System.out.println(line);
+    }
+    b.close();
+
+}
 }
--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,20 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriting{
-// Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+    // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+public static void main(String[] args)throws IOException {
+    FileWriter f = new FileWriter("C:\\Users\\sujat\\OneDrive\\Desktop\\s\\FileHandling_1\\Files.txt");
+    BufferedWriter br = new BufferedWriter(f);
+    br.write("sujatha");
+    br.newLine();
+    br.write("swarupa");
+    br.newLine();
+    br.write("kavitha");
+    br.newLine();
+    br.close();
 }
+}
+
