16222c25fbaa34b27063ded70f332b94400d7403
kashifakibria <kashifakibria@gmail.com>
2024-08-27 18:49:59 +0530

    FileReading and FileWriting

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,24 @@
+import java.io.*;
 public class FileReading{
 // Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+
+public static void main(String[] args)throws IOException {
+    try {
+        FileReader fr = new FileReader("Files.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        String s = br.readLine();
+        while (s != null) {
+            System.out.println(s);
+            s = br.readLine();
+        }
+
+        br.close();
+        //fr.close();
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 }
