5ddcfe10434002442ecffdf2c015a03620ae069e
2110Abhishek <agchoudhari2110@gmail.com>
2024-08-27 18:55:51 +0530

    Abhishek

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,18 @@
-public class FileReading{
-// Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReading {
+
+    public static void main(String[] args) throws IOException {      
+        FileReader fr = new FileReader("D:\\Java\\Regex_1\\FileHandling_1\\FileReading.java");
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        
+        br.close();
+    }
 }
--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,17 @@
-public class FileWriting{
-// Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriting
+{
+// Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+public static void main(String[] args) throws IOException
+{
+    FileWriter fr=new FileWriter("D:\\Java\\Regex_1\\FileHandling_1\\FileWriting.java");
+    BufferedWriter br=new BufferedWriter(fr);
+    br.write("Abhishek Ganpatrao Choudhari");
+    br.newLine();
+    br.write("Happy Birthday Abhishek =()");
+    br.close();
+}
 }
