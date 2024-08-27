642ae07901adb7eab20e5a8686d1f263b45a00fb
srinadh0405 <srinadh.nallabothula@email.com>
2024-08-27 19:46:32 +0530

    FileHandling

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,20 @@
+import java.io.*;
 public class FileReading{
 // Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+
+    public static void main(String[] args) {
+        try{
+            FileReader fr = new FileReader("E:\\Srinadh personals\\Sample Folders\\LogicalAssignmet\\FileHandling_1\\Files.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String string = br.readLine();
+            while(string!=null){
+                System.out.println(string);
+                string = br.readLine();
+            } 
+            br.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
