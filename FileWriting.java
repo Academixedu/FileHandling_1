74beba8a4ad8acde0dfdcb45a84ecff060cb3e64
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-08-27 20:30:01 +0530

    file

--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,15 @@
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+  
+    public static void main(String[] args)throws IOException{
+        FileWriter fw = new FileWriter("C:\\Users\\chokk\\Desktop\\practice\\FileHandling_1\\Files.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("java programming by using IO-pacakage");
+        bw.newLine();
+        bw.write("java programming by using Regex-pacakage");
+        bw.newLine();
+        bw.write("java programming by using Collections concept");
+        bw.newLine();
+        bw.close();
+    }
 }
