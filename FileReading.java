93a1edb7f524f2d839520669b49a60ed4ada2b38
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-27 18:17:48 +0530

    file

--- FileReading.java
+++ FileReading.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileReading{
+    public static void main(String[] args) throws IOException {
+        
+        FileReader fr=new FileReader("C:\\Users\\pavan\\OneDrive\\Pictures\\javabasics\\FileHandling_1\\Files.txt");
+        BufferedReader br=new BufferedReader(fr);
+        
+            String name="";
+            while((name=br.readLine())!=null){
+                System.out.println(name);
+
+            }
+            
+        
+
+        
+br.close();
+        
+    
 // Prepare a Logic to Read Data from File and Print IT USing Buffered Reader
+}
 }
