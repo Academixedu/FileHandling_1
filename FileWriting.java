7ef0f78e2a8a80d077cccc6446fd67db8e001df0
Ro-045 <pratapnaidu016@gmail.com>
2024-08-27 23:23:52 +0530

    pratap

--- FileWriting.java
+++ FileWriting.java
@@ -1,3 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
 public class FileWriting{
 // Prepare a Logic To Write the File and LOad data into File Using Buffered Writer
+public static void main(String[] args) throws IOException {
+    
+    String filePath = "C:\\Users\\HP\\OneDrive\\Desktop\\assign_6.000\\FileHandling_1\\Files.txt";
+    String data = "Prasad";
+    FileWriter fr=new FileWriter(filePath);
+    BufferedWriter br=new BufferedWriter(fr);
+    br.write(data);
+    br.newLine(); 
+    br.close(); 
+}
 }
